6bc25510abf03de278d4c28dd0ca7afd76c47e80
Mark Hamilton <mark.lee.hamilton@gmail.com>
2025-10-05 22:36:23 +0000

    figured out idempotency

--- celery/src/proj/tasks.py
+++ celery/src/proj/tasks.py
@@ -1,22 +1,29 @@
 import time
 from .celery import app
+import celery.utils.log
+
+logger = celery.utils.log.get_task_logger(__name__)
 
 
-@app.task
+@app.task(acks_late=True)
 def add(x, y):
+    logger.info(f"Adding {x} + {y}")
     return x + y
 
 
-@app.task
+@app.task(acks_late=True)
 def mul(x, y):
+    logger.info(f"Adding {x} x {y}")
     return x * y
 
 
-@app.task
+@app.task(acks_late=True)
 def xsum(numbers):
+    logger.info(f"sum {numbers}")
     return sum(numbers)
 
-@app.task
+@app.task(acks_late=True)
 def sleep(seconds):
+    logger.info(f"sleep {seconds} seconds")
     time.sleep(seconds)
     return seconds
