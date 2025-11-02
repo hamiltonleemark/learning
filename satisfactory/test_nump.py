3348fcbb09eca95508986ca80d81556e217f9ac5
Mark Hamilton <mark.lee.hamilton@gmail.com>
2025-11-02 15:30:52 -0800

    finished series

--- satisfactory/test_nump.py
+++ satisfactory/test_nump.py
@@ -31,18 +31,32 @@ def test_numpy_solve_linear():
 def test_numpy_name():
     """ Test the nump module functionality. """
 
-    rows = ["Plate", "Rod", "Screws", "Reinforced Plates",
-            "Plate output", "Rod output", "Screw output"]
+    rows = ["Ore output",
+            "Ingot", "Ingot output",
+            "Plate", "Rod", "Screws", "Reinforced Plate",
+            "Plate output", "Rod output", "Screw output",
+            "Reinforced Plate output"]
 
+    # Oo = Ore output
+    # I = Ingot
+    # Io = Ingot output
+
+    #    Oo,  I, Io,
     A = numpy.array([
-        [30, 15,  0,  0,  0,   0,  0],
-        [20, 0,   0,  0, -1,   0,  0],
-        [0,  0,  10,  0,  0,  -1,  0],
-        [0,  0,   0, 10,  -1,  0,  0],
-        [0,  15,  0,  0,   0,  -1, 0],
-        [0,   0, 40,  0,   0,   0, -1],
-        [0,   0,  0, 60,   0,   0, -1]])
-    B = numpy.array([120, 0, 0, 0, 0, 0, 0], )
+        [ 1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0],  # Oo = 120
+        [ -1, 30, 0,  0,  0,  0,  0,  0,  0,  0,  0],  # 30*I = Oo
+        [ 0,  30,-1,  0,  0,  0,  0,  0,  0,  0,  0],  # Io = 30*I
+        [ 0,  0, -1, 30, 15,  0,  0,  0,  0,  0,  0],
+        [ 0,  0,  0, 20,  0,  0,  0, -1,  0,  0,  0],
+        [ 0,  0,  0,  0,  0, 10,  0,  0, -1,  0,  0],
+        [ 0,  0,  0,  0,  0,  0, 10, -1,  0,  0,  0],
+        [ 0,  0,  0,  0, 15,  0,  0,  0, -1,  0,  0],
+        [ 0,  0,  0,  0,  0, 40,  0,  0,  0, -1,  0],
+        [ 0,  0,  0,  0,  0,  0, 60,  0,  0, -1,  0],
+        [ 0,  0,  0,  0,  0,  0, 40,  0,  0,  0, -1]
+    ])
+
+    B = numpy.array([120, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
     x = numpy.linalg.solve(A, B)
     print(x)
