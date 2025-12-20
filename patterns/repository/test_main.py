6a45a5e592f64fb8baf6c4c3fe24cb4a13039699
Mark Hamilton <mark.lee.hamilton@gmail.com>
2025-12-20 15:26:33 -0800

    capture learning from deeplearning ai

--- patterns/repository/test_main.py
+++ patterns/repository/test_main.py
@@ -1,41 +1,11 @@
-"""
-Unit test of main.
-"""
-import pytest
-from main import Repository
+import main
 
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
-# pylint: disable=redefined-outer-name
-
-class TestRepository:
-    def __init__(self):
-        self.data_store = {}
-
+class ConcreteRepository(main.Repository):
     def save(self, data: dict) -> None:
-        self.data_store.update(data)
+        print(f"Saving data: {data}")
 
-    def get_data(self):
-        return self.data_store
 
-@pytest.fixture
-def repo():
-    return TestRepository()
+def test_repository_save():
 
-def test_save_single_item(repo):
-    repo.save({'key1': 'value1'})
-    assert repo.get_data() == {'key1': 'value1'}
-
-def test_save_multiple_items(repo):
-    repo.save({'key1': 'value1'})
-    repo.save({'key2': 'value2'})
-    assert repo.get_data() == {'key1': 'value1', 'key2': 'value2'}
-
-def test_overwrite_item(repo):
-    repo.save({'key1': 'value1'})
-    repo.save({'key1': 'new_value'})
-    assert repo.get_data() == {'key1': 'new_value'}
-
-def test_save_empty_dict(repo):
-    repo.save({})
-    assert repo.get_data() == {}
+    repo = ConcreteRepository()
+    repo.save({"key": "value"})
