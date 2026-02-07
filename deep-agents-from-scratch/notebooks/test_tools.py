38f797e5631f38da1cbc7165c757aeb6ae7b272b
Mark Hamilton <mark.lee.hamilton@gmail.com>
2026-02-06 18:47:06 -0800

    work in progress

---
+++ deep-agents-from-scratch/notebooks/test_tools.py
@@ -0,0 +1,12 @@
+import pytest 
+from notebooks import utils
+
+
+def test_reducer_syntax():
+    """ Learning how to user reducer. """
+
+    dict1 = {'a': 1, 'b': 2, 'c': 3}
+    dict2 = {'d': 4, 'e': 5, 'f': 6}
+
+    dict3 = {**dict1, **dict2}
+    print(dict3)
